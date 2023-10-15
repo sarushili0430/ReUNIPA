d70c9224184aa891be1e269208d134fcc4c85795
sarushili0430 <kouyuu6@gmail.com>
2023-10-15 23:37:22 +0900

    MSTeams login support

--- login.py
+++ login.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome import service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,8 +14,8 @@ import time
 CHROMEDRIVER = "chromedriver.exe"
 SIGNINURL = "https://upweb3.kuas.ac.jp/uprx/"
 
-ID = "2022m089"
-PWD = "Saru0430"
+ID = "XXXXXXXX"
+PWD = "XXXXXXXX"
 
 #Setting up the selenium browser
 chrome_service = service.Service(executable_path=CHROMEDRIVER)
@@ -41,7 +42,7 @@ options.add_argument("--start-maximized")
 #options.add_argument("--headless")
 
 
-class Login():
+class UNIPA_Login():
 
     def __init__(self,ID,PWD):
         self.id = ID
@@ -95,6 +96,38 @@ class Login():
             except:
                 pass
 
+        self.driver.quit()
         return assignment_list
+
+class Teams_Login():
+
+    def __init__(self,ID,PWD):
+        self.id = ID
+        self.pwd = PWD
+        #Opening the Chrome browser
+        self.driver = webdriver.Chrome(service=chrome_service)
+        self.wait = WebDriverWait(self.driver,timeout=30)
+        self.driver.get(url="https://teams.microsoft.com/")
+
+    def login(self):
+        #Login to account
+        self.wait.until(EC.presence_of_element_located((By.ID,"i0116")))
+        #time.sleep(10)
+        username_input = self.driver.find_element(By.ID,"i0116")
+        username_input.send_keys(self.id)
+        next_btn = self.driver.find_element(By.ID,"idSIButton9")
+        next_btn.click()     
+        self.wait.until(EC.presence_of_element_located((By.ID,"i0118")))
+        pwd_input = self.driver.find_element(By.ID,"i0118")
+        pwd_input.send_keys(self.pwd)
+        time.sleep(5)
+        pwd_input.send_keys(Keys.ENTER)
+        #login_btn = self.driver.find_element(By.ID,"idSIButton9")
+        #login_btn.click()
+        #self.wait.until(EC.element_to_be_clickable((By.ID,"idSIButton9")))
+        time.sleep(100)
+        #login_btn = self.driver.find_element(By.ID,"idSIButton9")
+        #self.wait.until(EC.staleness_of(login_btn))
+
 
     
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import login
 
-ID = "2022m089"
-PWD = "Saru0430"
+ID = "XXXXXXXX"
+PWD = "XXXXXXXX"
 
-a = login.Login(ID,PWD)
+
+a = login.UNIPA_Login(ID,PWD)
 a.login()
 assignments = a.get_assignment()
 
+b = login.Teams_Login("2022m089@kuas.ac.jp","Saru0430")
+b.login()
 print(assignments)
